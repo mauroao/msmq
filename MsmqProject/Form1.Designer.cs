fe46eb44562f8ca0b7a75e8c56e0a8df1a56315c
mauroao <mauro.anselmo.oliveira@gmail.com>
2018-02-06 17:31:37 -0800

    first commit

---
+++ MsmqProject/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MsmqProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLabelSend = new System.Windows.Forms.TextBox();
+            this.txtBodySend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.txtBodyReceive = new System.Windows.Forms.TextBox();
+            this.txtLabelReceive = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLabelSend
+            // 
+            this.txtLabelSend.Location = new System.Drawing.Point(41, 38);
+            this.txtLabelSend.Name = "txtLabelSend";
+            this.txtLabelSend.Size = new System.Drawing.Size(339, 20);
+            this.txtLabelSend.TabIndex = 0;
+            // 
+            // txtBodySend
+            // 
+            this.txtBodySend.Location = new System.Drawing.Point(41, 76);
+            this.txtBodySend.Name = "txtBodySend";
+            this.txtBodySend.Size = new System.Drawing.Size(339, 20);
+            this.txtBodySend.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.Control;
+            this.btnSend.Location = new System.Drawing.Point(305, 114);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(305, 260);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 5;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // txtBodyReceive
+            // 
+            this.txtBodyReceive.Location = new System.Drawing.Point(41, 222);
+            this.txtBodyReceive.Name = "txtBodyReceive";
+            this.txtBodyReceive.Size = new System.Drawing.Size(339, 20);
+            this.txtBodyReceive.TabIndex = 4;
+            // 
+            // txtLabelReceive
+            // 
+            this.txtLabelReceive.Location = new System.Drawing.Point(41, 184);
+            this.txtLabelReceive.Name = "txtLabelReceive";
+            this.txtLabelReceive.Size = new System.Drawing.Size(339, 20);
+            this.txtLabelReceive.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 312);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.txtBodyReceive);
+            this.Controls.Add(this.txtLabelReceive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtBodySend);
+            this.Controls.Add(this.txtLabelSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLabelSend;
+        private System.Windows.Forms.TextBox txtBodySend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.TextBox txtBodyReceive;
+        private System.Windows.Forms.TextBox txtLabelReceive;
+    }
+}
+
---
+++ MsmqProject/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MsmqProject
+{
+    public partial class Form1 : Form
+    {
+        private System.Messaging.MessageQueue msmq;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // var path = @"FormatName:DIRECT=OS:desktop-6hsukeq\private$\myfirstqueue";
+            var path = @"FormatName:DIRECT=TCP:192.168.1.20\private$\myfirstqueue";
+            msmq = new System.Messaging.MessageQueue(
+                path, 
+                false, 
+                true, 
+                System.Messaging.QueueAccessMode.SendAndReceive
+            );
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var labelText = txtLabelSend.Text;
+            var bodyText = txtBodySend.Text;
+
+            msmq.Send(bodyText, labelText);
+
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            var timeOut = 1000;
+
+            var message = msmq.Receive(new TimeSpan(timeOut));
+
+            message.Formatter = new System.Messaging.XmlMessageFormatter(new Type[] { typeof(String) });
+
+            txtLabelReceive.Text = message.Label;
+            txtBodyReceive.Text = (String)message.Body;
+        }
+    }
+}
